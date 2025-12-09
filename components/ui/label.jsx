33941a55b0e96efd42f8e48e1a1af6f4da2458d1
songshanshan1977-cmyk <songshanshan1977@gmail.com>
2025-12-09 12:02:13 +0900

    Create label.jsx

---
+++ components/ui/label.jsx
@@ -0,0 +1,7 @@
+export default function Label({ children, className = "", ...props }) {
+  return (
+    <label className={`block text-sm font-medium mb-1 ${className}`} {...props}>
+      {children}
+    </label>
+  );
+}
