43037d00c4fccef844915d51362be36624b860cb
songshanshan1977-cmyk <songshanshan1977@gmail.com>
2025-12-10 10:40:11 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontSize: "22px" }}>
+      <h1>Okinawa Booking System</h1>
+      <p>Homepage is working.</p>
+
+      <a href="/booking" style={{ color: "blue" }}>
+        Go to Booking →
+      </a>
+    </div>
+  );
+}
